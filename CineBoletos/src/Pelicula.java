99e4618b57bc6b811f0ece05caf51c2daa54a603
UrielFl-Ch <000064710@alumnos.icel.edu.mx>
2024-12-14 20:12:38 -0600

    clase pelicula acabado.

--- CineBoletos/src/Pelicula.java
+++ CineBoletos/src/Pelicula.java
@@ -7,16 +7,16 @@
  *
  * @author Pc
  */
-import java.io.Serializable;
+import java.io.Serializable; //con esto los objetos de la clse 'pelicula' se pueden serializar
 
-public class Pelicula implements Serializable {
-    private final String nombre;
+public class Pelicula implements Serializable { // publico para su facil acceso desde otras clases
+    private final String nombre; //nombre almacebara el nombre de la pelicula
 
-    public Pelicula(String nombre) {
+    public Pelicula(String nombre) { // Definiendo el contructor
         this.nombre = nombre;
     }
 
-    public String getNombre() {
+    public String getNombre() { // Devuelve el valor de 'nombre'
         return nombre;
     }
 }
